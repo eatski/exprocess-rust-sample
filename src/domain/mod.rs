2f37b7ba4131585f7f562bf629427f060e5258fd
hagaitski <i.haga629@gmail.com>
2021-10-21 03:48:25 +0900

    fix: 解放

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -5,6 +5,6 @@ pub type Runner = expro::exprocess::Runner<exprocess::AppCore>;
 
 pub fn start(room_id: String,listener: expro::exprocess::Listener<exprocess::AppCore,exprocess::AppState>) -> Runner {
     let repo = repository::AppRepository::new(room_id);
-    // let repo = expro::stacked::DirectlyDispatch::wrap(repo);
+    let repo = expro::stacked::DirectlyDispatch::wrap(repo);
     Runner::start(repo,listener)
 }
