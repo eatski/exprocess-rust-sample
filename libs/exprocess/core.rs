bd3cd6f4e8863362c5c2840b32e4087bae188c06
hagaitski <i.haga629@gmail.com>
2021-11-04 17:25:02 +0900

    fix: doc

--- libs/exprocess/core.rs
+++ libs/exprocess/core.rs
@@ -1,36 +1,91 @@
 pub trait ExprocessCore {
+
     type State;
     type Command;
     type Result;
+    ///
+    /// Initializes and returns the state of the application.
+    /// 
     fn init() -> Self::State;
-    fn resolve(prev: &Self::State, command: Self::Command) -> Self::Result;
-    fn reducer(prev: &mut Self::State, result: Self::Result);
+    ///
+    /// Returns a Result from Command and the current state.
+    /// This function does not need to be idempotent. 
+    /// For example, it is possible to incorporate a random number into the logic.
+    /// 
+    fn resolve(state: &Self::State, command: Self::Command) -> Self::Result;
+    ///
+    /// Update the state of the application from Result.
+    /// This operation must be idempotent.
+    /// By the idempotence, Exprocess can reconstruct the state of the application from the history of Result and this function.
+    /// 
+    fn reducer(state: &mut Self::State, result: Self::Result);
 }
+
+
+/* sample code */
 
 struct SampleCore;
 
 struct SampleState {
-    hoge: Vec<String>,
-    fuga: Vec<String>
+    pub health: i32,
+    pub sociability: Sociability
+}
+
+enum Sociability {
+    #[allow(dead_code)]
+    Popular,
+    Solitude
+}
+
+enum SampleCommand {
+    #[allow(dead_code)]
+    EatSteak,
+    #[allow(dead_code)]
+    MeetFriend
+}
+
+enum SampleResult {
+    FeelBetter(i32),FeelWorse(i32)
 }
 
 impl ExprocessCore for SampleCore {
     type State = SampleState;
 
-    type Command = ();
+    type Command = SampleCommand;
 
-    type Result = ();
+    type Result = SampleResult;
 
     fn init() -> Self::State {
-        todo!()
+        SampleState {
+            health: 100,
+            sociability: Sociability::Solitude
+        }
     }
 
-    fn resolve(_prev: &Self::State, _command: Self::Command) -> Self::Result {
-        todo!()
+    fn resolve(state: &Self::State, command: Self::Command) -> Self::Result {
+        match command {
+            SampleCommand::EatSteak => {
+                SampleResult::FeelBetter(10)
+            },
+            SampleCommand::MeetFriend => {
+                match &state.sociability {
+                    Sociability::Popular => SampleResult::FeelBetter(30),
+                    Sociability::Solitude => SampleResult::FeelWorse(10),
+                }
+                
+            },
+        }
     }
 
-    fn reducer(prev: &mut Self::State, _result: Self::Result) {
-        prev.fuga =  prev.hoge.drain(0..).collect();
+    fn reducer(state: &mut Self::State, result: Self::Result) {
+        match result {
+            SampleResult::FeelBetter(helth) => {
+                state.health = helth;
+            },
+            SampleResult::FeelWorse(helth) => {
+                state.health = -helth;
+            },
+        }
     }
 
 }
