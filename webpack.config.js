0f9ccce3ff72d9832296e2c3fde1307590c0b552
hagaitski <i.haga629@gmail.com>
2021-11-30 12:22:29 +0900

    fix: typo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 
 const BUILD_MODE = "BUILD_MODE";
