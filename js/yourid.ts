bbb64aeedf72de79c8aef77f08a8e0ac089db369
hagaitski <i.haga629@gmail.com>
2021-12-16 13:31:53 +0900

    fix: inmemory

--- js/yourid.ts
+++ js/yourid.ts
@@ -1,5 +1,21 @@
 
 const toYourIdKey = (roomId: string) => `${roomId}:yourid`;
-export const getYourId = (roomId:string) => window.localStorage.getItem(toYourIdKey(roomId));
 
-export const setYourId = (roomId:string,yourId: string) => window.localStorage.setItem(toYourIdKey(roomId),yourId);
+export const getYourId = (roomId:string) => {
+    if(process.env.BUILD_MODE === "dev") {
+        //@ts-expect-error
+        return window.developmentYourId;
+    } else {
+        return window.localStorage.getItem(toYourIdKey(roomId)) 
+    }
+};
+
+export const setYourId = (roomId:string,yourId: string) => {
+    if(process.env.BUILD_MODE === "dev") {
+        //@ts-expect-error
+        window.developmentYourId = yourId;
+    } else {
+        window.localStorage.setItem(toYourIdKey(roomId),yourId)
+    }
+    
+};
